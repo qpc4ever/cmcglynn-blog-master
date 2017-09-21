ee3ae1b98d1a5dcd80ac0fb93e03923a5a47bc3d
qpc4ever <qpc4ever@gmail.com>
2017-09-21 16:20:22 -0400

    second commit
    my helper class addition

---
+++ cmcglynn-blog/Models/mytesthelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cmcglynn_blog.Models
+{
+    public class mytesthelper
+    {
+        public int mytest()
+        {
+            var test = 1 + 2;
+            return test;
+        }
+    }
+}
